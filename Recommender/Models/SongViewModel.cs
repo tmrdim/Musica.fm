44c9036eb0ea8f2c14b15aee30fc900ffc193769
comrade1992 <mende.tosheski@gmail.com>
2015-06-04 19:27:58 +0200

    UserCollection

---
+++ Recommender/Models/SongViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recommender.Models
+{
+    public class SongsViewModel
+    {
+        public int SongId { get; set; }
+
+        public string SongTitle { get; set; }
+
+        public int ArtistId { get; set; }
+
+        public int? AuthorId { get; set; }
+
+        public int GenreId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public double? Duration { get; set; }
+
+        public int CollectionId { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string GenreName { get; set; }
+    }
+}
